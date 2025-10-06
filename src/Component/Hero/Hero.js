652d36de5633a6cae11c1c0520fd037a80ed36e7
Akash-develop <akash.p02.dev@gmail.com>
2025-10-06 16:37:03 +0530

    update-resume-changes

--- src/Component/Hero/Hero.js
+++ src/Component/Hero/Hero.js
@@ -3,12 +3,21 @@ import React from "react";
 import styles from "./Hero.module.css";
 import Akash from "../../assets/hero/Akash.png";
 export const Hero = () => {
+
+  const about = `Hi, I’m Akash from Ayanavaram, Chennai.
+As a software developer I have 3 years of hands-on experience in React.js, specializing in building reusable, readable, and optimized web applications for better performance.
+
+I’ve also worked with LLMs (Large Language Models) and API integrations to develop AI-powered features. Recently, I implemented RAG (Retrieval-Augmented Generation) to enhance data interaction and feed dynamic information into AI systems for smarter responses.
+
+At DigiVal IT Solutions, I improved application performance by 30% and reduced database query execution time by 40% through thoughtful design and optimization. My background in CAD design has honed my analytical and problem-solving skills, enabling me to deliver innovative solutions for complex web projects.
+
+I'm always eager to explore new technologies and best practices—especially in ReactJS, DevOps, and cloud-native development (Docker, Kubernetes). Let's connect if you want to build robust, user-centric digital products or discuss the latest in frontend engineering!`
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Akash</h1>
         <p className={styles.description}>
-         Experienced React and Full-Stack Developer with 3.5+ years of combined technical experience. Developed scalable features using JavaScript (ES6+), React.js, Redux, and RESTful APIs, improving application performance by 30% and reducing database query execution time by 40%. Strong analytical background from CAD design experience, enabling innovative solutions for complex web applications and responsive UI development.
+          {about}
         </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
           Contact Me
